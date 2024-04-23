54f4030670a793365308c3a760e944f54325659f
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-04-23 13:40:33 -0600

    Table creation
    Created table function

--- main.py
+++ main.py
@@ -18,6 +18,18 @@
 # 
 #
 ################################################################################
+import pandas as pd
+import numpy as np;
+
+def table(file_path):
+    ''' Loads data from .csv file into a panda table'''
+    # Load the .csv file into a panda table
+    
+    data = pd.read_csv(file_path)
+    print(data.head())
 
 
-print("Hello World!")
+
+
+if __name__ == "__main__":
+    table('ultrasoundData.csv')
